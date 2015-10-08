7562ccc441794f97d1b8c823d1fa019f2d6afaa5
babilonsuxx <babilonsuxx@gmail.com>
2015-10-08 11:24:36 +0300

    открыли сокет

--- joystick-pc/joystick-pc/Form1.cs
+++ joystick-pc/joystick-pc/Form1.cs
@@ -2,27 +2,31 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Net;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace joystick_pc
 {
     public partial class Form1 : Form
     {
+       
+        
         public Form1()
         {
             InitializeComponent();
-            //получаем текущий ip
 
-// Получение имени компьютера.
-String host = System.Net.Dns.GetHostName();
-// Получение ip-адреса.
-System.Net.IPAddress ip = System.Net.Dns.GetHostByName(host).AddressList[0];
-// Показ адреса в label'е.
-labelIp.Text = ip.ToString();
+
+            // Получение имени компьютера.
+            String host = System.Net.Dns.GetHostName();
+            // Получение ip-адреса.
+            System.Net.IPAddress ip = System.Net.Dns.GetHostByName(host).AddressList[0];
+            // Показ адреса в label'е.
+            labelIp.Text = ip.ToString();
         }
 
        
@@ -36,9 +40,12 @@ labelIp.Text = ip.ToString();
                 btnStart.Text = "STOP";
                 btnStart.ForeColor = System.Drawing.Color.Red;
                 //номер порта
-                int portNum = Convert.ToInt32(textBox1.text);
+                int portNum = Convert.ToInt32(textBox1.Text);
                 // открываем сокет
-
+                String host = System.Net.Dns.GetHostName();
+                IPAddress ipAddr = System.Net.Dns.GetHostByName(host).AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, portNum);
+                Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
             }
             else
@@ -47,6 +54,8 @@ labelIp.Text = ip.ToString();
                 btnStart.Text = "START";
                 btnStart.ForeColor = System.Drawing.Color.Green;
                 //останавливаем 
+
+
             }
             
         }
